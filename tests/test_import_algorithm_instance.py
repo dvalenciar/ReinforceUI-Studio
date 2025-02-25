b03cd15f0719b249eab9e03139ee658c2b76c0d9
david <davidvalenciaredrovan@gmail.com>
2025-02-25 19:22:36 +1300

    ppo ready

--- tests/test_import_algorithm_instance.py
+++ tests/test_import_algorithm_instance.py
@@ -4,5 +4,5 @@ from RL_loops.training_policy_loop import import_algorithm_instance
 
 def test_import_algorithm_instance():
     config_data = {"Algorithm": "TD3"}
-    algorithm_class = import_algorithm_instance(config_data)
+    algorithm_class, algorithm_name = import_algorithm_instance(config_data)
     assert algorithm_class.__name__ == "TD3"
