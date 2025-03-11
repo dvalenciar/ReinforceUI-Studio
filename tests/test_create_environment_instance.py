c29504e7a739e56809b387b420eab7a0cf470fd8
david <davidvalenciaredrovan@gmail.com>
2025-03-12 12:16:43 +1300

    reformat test functions

--- tests/test_create_environment_instance.py
+++ tests/test_create_environment_instance.py
@@ -1,11 +1,26 @@
+import os
+import yaml
 from RL_loops.training_policy_loop import create_environment_instance
 
 
 def test_create_environment_instance():
-    config_data = {
-        "selected_platform": "Gymnasium",
-        "selected_environment": "Pendulum-v1",
-        "Seed": 42,
-    }
-    environment = create_environment_instance(config_data)
-    assert environment.env.spec.id == "Pendulum-v1"
+    config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'config_platform.yaml')
+
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    for platform, envs in config['platforms'].items():
+        all_envs = envs.get('environments', []) + envs.get('discrete_environments', [])
+        for env in all_envs:
+            config_data = {
+                "selected_platform": platform,
+                "selected_environment": env,
+                "Seed": 42,
+            }
+            environment = create_environment_instance(config_data)
+
+            assert environment is not None, f"Failed to create environment for {platform} - {env}"
+
+            assert hasattr(environment, 'reset'), "Environment instance should have a reset method"
+            assert hasattr(environment, 'step'), "Environment instance should have a step method"
+            assert hasattr(environment, 'sample_action'), "Environment instance should have a sample_action method"
--- tests/test_import_algorithm_instance.py
+++ tests/test_import_algorithm_instance.py
@@ -1,8 +1,11 @@
-# tests/test_import_algorithm_instance.py
 from RL_loops.training_policy_loop import import_algorithm_instance
 
 
 def test_import_algorithm_instance():
-    config_data = {"Algorithm": "TD3"}
-    algorithm_class, algorithm_name = import_algorithm_instance(config_data)
-    assert algorithm_class.__name__ == "TD3"
+    algorithms = ["CTD4", "DDPG", "DQN", "PPO", "SAC", "TD3", "TQC"]
+    for algorithm in algorithms:
+        config_data = {"Algorithm": algorithm}
+        algorithm_class, algorithm_name = import_algorithm_instance(config_data)
+        assert algorithm_class.__name__ == algorithm
+        assert algorithm_name == algorithm
+
