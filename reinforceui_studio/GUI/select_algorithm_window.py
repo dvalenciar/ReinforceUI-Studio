5de7666789937eecdee8bd3cc1d3c67fe215c8ba
david <davidvalenciaredrovan@gmail.com>
2025-07-08 16:38:47 +1200

    remove redundant button style update comment in select_algorithm_window.py

--- reinforceui_studio/GUI/select_algorithm_window.py
+++ reinforceui_studio/GUI/select_algorithm_window.py
@@ -198,7 +198,6 @@ class SelectAlgorithmWindow(BaseWindow):
         msg_box.setStandardButtons(QMessageBox.Ok)
         msg_box.exec_()
 
-    # todo this is redundant with the one in update_button_styles in training_window.py, remove it or make it a common function
     def set_active_button(self, active_button, inactive_button) -> None:  # noqa
         """Visually highlight the selected option button."""
         active_button.setStyleSheet(Styles.SELECTED_BUTTON)
